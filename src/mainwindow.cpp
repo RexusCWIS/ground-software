f23bde79636e3ee79cd0e2b500450424daba3fa7
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-12-02 23:39:32 +0100

    Main window: added a central widget and the status tab.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,8 +3,15 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
+    m_centralWidget = new QWidget(this);
+    m_statusTab = new StatusTab(m_centralWidget);
+    m_layout = new QGridLayout(m_centralWidget);
+    m_layout->addWidget(m_statusTab, 0, 0);
+    m_centralWidget->setLayout(m_layout);
+
+    this->setCentralWidget(m_centralWidget);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,14 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGridLayout>
 
-class MainWindow : public QMainWindow
-{
+#include "statustab.h"
+
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
+
+    private:
+        StatusTab *m_statusTab;
+        QGridLayout *m_layout;
+        QWidget *m_centralWidget;
 };
 
 #endif // MAINWINDOW_H
